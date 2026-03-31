ed437cfdd3bc372b176c2b3a3de4378bbe873a40
dnomokonov <zmnomokon715@gmail.com>
2026-03-31 11:18:25 +0700

    Update Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import {Outlet} from 'react-router-dom';
 import {useDispatch} from 'react-redux';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import {setOffers} from './store/offersSlice.ts';
 import {offers} from './mocks/offers.ts';
 
 export default function App() {
   const dispatch = useDispatch();
+  const [isUploadData, setUploadData] = useState(false);
 
   useEffect(() => {
     dispatch(setOffers(offers));
+    setUploadData(true);
   }, [dispatch]);
+
+  if (!isUploadData) {
+    return null;
+  }
 
   return (
     <Outlet />
--- src/pages/OfferPage/OfferPage.tsx
+++ src/pages/OfferPage/OfferPage.tsx
@@ -1,21 +1,30 @@
 import {ReviewForm} from '../../components/ReviewForm/ReviewForm.tsx';
-import {Link, useParams} from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
 import {AppRoute} from '../../const.ts';
 import {ReviewsList} from '../../components/ReviewsList/ReviewsList.tsx';
 import {REVIEWS_MOCK} from '../../mocks/reviews.ts';
 import {Map} from '../../components/Map/Map.tsx';
 import {OffersList} from '../../components/OffersList/OffersList.tsx';
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import {Offer} from '../../types/offer.ts';
 import {useSelector} from 'react-redux';
 import {selectAllOffers} from '../../store/offersSelectors.ts';
 
-export function OfferPage() : JSX.Element {
+export function OfferPage() {
   const [activeOffer, setActiveOffer] = useState<Offer | null>(null);
-  const offers = useSelector(selectAllOffers);
+  const navigate = useNavigate();
 
   const { id } = useParams();
-  const filteredOffers = offers.filter((offer: Offer | null) => offer?.id !== id);
+  const offers = useSelector(selectAllOffers);
+  const currentOffer = offers.find((offer) => offer.id === id);
+
+  useEffect(() => {
+    if (!currentOffer) {
+      navigate(AppRoute.NotFound);
+    }
+  }, [currentOffer, navigate]);
+
+  const filteredOffers = offers.filter((offer: Offer) => offer.id !== id);
   const cityForMap = filteredOffers.length > 0 ? filteredOffers[0].city : null;
 
   const handleCardHover = (offer: Offer | null) => {
