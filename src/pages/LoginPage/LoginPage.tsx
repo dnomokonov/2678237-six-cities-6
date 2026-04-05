f34ca5973d3c1cbda241c0ab9977718aa09c01a2
dnomokonov <zmnomokon715@gmail.com>
2026-04-05 13:20:12 +0700

    Add component Header

--- src/pages/LoginPage/LoginPage.tsx
+++ src/pages/LoginPage/LoginPage.tsx
@@ -1,21 +1,9 @@
-import {Link} from 'react-router-dom';
-import {AppRoute} from '../../const.ts';
+import {HeaderLogin} from '../../components/Header/HeaderLogin.tsx';
 
 export function LoginPage() {
   return (
     <div className="page page--gray page--login">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <Link to={AppRoute.Root} className="header__logo-link">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </Link>
-            </div>
-          </div>
-        </div>
-      </header>
-
+      <HeaderLogin />
       <main className="page__main page__main--login">
         <div className="page__login-container container">
           <section className="login">
--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,9 +1,8 @@
-import {Link} from 'react-router-dom';
 import {OffersList} from '../../components/OffersList/OffersList.tsx';
 import {Offer} from '../../types/offer.ts';
 import {useMemo, useState} from 'react';
 import {Map} from '../../components/Map/Map.tsx';
-import {AppRoute, CITIES, sortingMap} from '../../const.ts';
+import {CITIES, sortingMap} from '../../const.ts';
 import {ListCity} from '../../components/ListCity/ListCity.tsx';
 import {useDispatch, useSelector} from 'react-redux';
 import {selectAllOffers, selectCurrentCity, selectCurrentSortOption} from '../../store/offersSelectors.ts';
@@ -11,6 +10,7 @@ import {editCity} from '../../store/offersSlice.ts';
 import {SortDropdown} from '../../components/SortDropdown/SortDropdown.tsx';
 import {fetchOffersAction} from '../../store/api-actions.ts';
 import store from '../../store';
+import {Header} from '../../components/Header/Header.tsx';
 
 store.dispatch(fetchOffersAction());
 
@@ -40,35 +40,7 @@ export function MainPage() {
 
   return (
     <div className="page page--gray page--main">
-      <header className="header">
-        <div className="container">
-          <div className="header__wrapper">
-            <div className="header__left">
-              <a className="header__logo-link header__logo-link--active">
-                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41"/>
-              </a>
-            </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <Link to='#' className="header__nav-link header__nav-link--profile">
-                    <div className="header__avatar-wrapper user__avatar-wrapper">
-                    </div>
-                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
-                    <span className="header__favorite-count">3</span>
-                  </Link>
-                </li>
-                <li className="header__nav-item">
-                  <Link to={AppRoute.Root} className="header__nav-link">
-                    <span className="header__signout">Sign out</span>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
